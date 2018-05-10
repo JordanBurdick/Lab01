53093779ea9d6baedb761d3d750d4e7f89dc406c
JordanBurdick <31458582+JordanBurdick@users.noreply.github.com>
2018-05-10 14:04:06 -0400

    Add files via upload

---
+++ Lab 1/Lab01.1JordanBurdick/Lab01.1JordanBurdick/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01._1JordanBurdick
+{
+    class Program
+    {
+        public static string UserName = "Jordan Burdick";
+        public static string Greeting = "Hello World";
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("{0} from {1}, I am speaking to you through the Console.WriteLine function", Greeting, UserName);
+            Console.ReadKey(true);
+
+
+            //pls 
+        }
+    }
+}
